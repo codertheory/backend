45bcf3c509c56c3766cff7d7f6ca6f538f042ccd
lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-01-28 19:16:14 -0700

    Updated app.json Updated production/staging configs

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import *  # noqa
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["codertheory.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["codertheory.dev"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -71,7 +71,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="codertheory <noreply@codertheory.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="codertheory <noreply@codertheory.dev>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
@@ -153,7 +153,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [env.cache("REDIS_URL")],
         },
     },
 }
