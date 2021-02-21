c4daeed2a409510f7cc9fb2d5d9722040b5574b2
lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-02-20 19:35:01 -0700

    [shiritori] Updates
    - updated lobby_consumer tests to sleep to avoid sqlite errors

--- codertheory/shiritori/tests/test_lobby_consumer.py
+++ codertheory/shiritori/tests/test_lobby_consumer.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from channels.db import database_sync_to_async
 from channels.testing import WebsocketCommunicator
 from django.test import TransactionTestCase
@@ -37,6 +39,7 @@ class LobbyConsumerTests(TransactionTestCase):
 
     async def test_on_game_created(self):
         communicator = await self._setUpData()
+        await asyncio.sleep(1)
         response = await communicator.receive_json_from()
         data = response['data']['games']
         self.assertEqual(len(data), 0)
@@ -50,12 +53,15 @@ class LobbyConsumerTests(TransactionTestCase):
     async def test_on_game_started(self):
         communicator = await self._setUpData()
         await communicator.receive_json_from()
+        await asyncio.sleep(1)
         games = await self._create_games(1)
+        await asyncio.sleep(1)
         await communicator.receive_json_from()
+        await communicator.receive_json_from()  # This is a game_updated call due to GameFactory's post_generation
 
         await self.save_game(games[0])
         new_response = await communicator.receive_json_from()
-        self.assertEqual(new_response['type'], ShiritoriEvents.GameStarted.value)
+        self.assertEqual(new_response['type'], ShiritoriEvents.GameStarted)
         data = new_response['data']
         self.assertEqual(data['id'], games[0].id)
         await communicator.disconnect()
