7fbb552a51b3116b93bef886afa91575b5f91e17
lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-03-26 11:13:27 -0600

    Fixed settings and updated

--- config/settings/base.py
+++ config/settings/base.py
@@ -469,7 +469,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [env.cache("REDIS_URL")],
+            "hosts": [env.str('REDIS_URL')],
         },
     }
 }
