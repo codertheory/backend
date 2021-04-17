b4a426b792fac3a4e98991e467283365bcb9db55
lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-04-16 21:56:51 -0600

    more cleanup

--- config/settings/base.py
+++ config/settings/base.py
@@ -335,6 +335,7 @@ REST_FRAMEWORK = {
         'anon': '500/day',
         'user': '1000/day'
     },
+    'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE': 50,
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning'
 }
@@ -381,7 +382,6 @@ if SENTRY_DSN:
         dsn=SENTRY_DSN,
         integrations=[sentry_logging, DjangoIntegration(), RedisIntegration()],
     )
-
 
 # CHANNELS
 # ------------------------------------------------------------------------------
