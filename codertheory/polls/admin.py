c065d06219401d3c56ebc9cc6e2d5ff9ce312cfe
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-06-15 16:25:57 -0600

    Updated Poll admin.py

--- codertheory/polls/admin.py
+++ codertheory/polls/admin.py
@@ -8,14 +8,17 @@ from codertheory.polls import models
 
 @admin.register(models.Poll)
 class PollAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
 
 
 @admin.register(models.PollOption)
 class PollOptionAdmin(admin.ModelAdmin):
+    list_display = ["option", "poll"]
     list_filter = ("poll",)
+    search_fields = ['poll__id', 'option']
 
 
 @admin.register(models.PollVote)
 class PollVoteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["option", "poll"]
+    search_fields = ['poll__id', 'option__name']
