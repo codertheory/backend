aeb80b3e6c0f289a2bd146695e8adec5a6864745
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-06-20 09:57:37 -0600

    Updated PollSubscription methods to be async

--- codertheory/polls/graphql/subscriptions.py
+++ codertheory/polls/graphql/subscriptions.py
@@ -1,7 +1,9 @@
-import time
+import asyncio
+import typing
 
 import channels_graphql_ws
 import graphene
+from channels.db import database_sync_to_async
 
 from codertheory.polls import models
 from codertheory.polls.graphql.types import PollType
@@ -14,14 +16,19 @@ class PollSubscription(channels_graphql_ws.Subscription):
         id = graphene.ID(description="ID of the Poll")
 
     @staticmethod
-    def subscribe(root, info, id=None):
+    async def subscribe(root, info, id=None):
         """Called when user subscribes."""
 
         # Return the list of subscription group names.
         return [id]
 
     @staticmethod
-    def publish(payload, info, id):
+    @database_sync_to_async
+    def get_poll(id) -> typing.Awaitable[models.Poll]:
+        return models.Poll.objects.get(pk=id)
+
+    @staticmethod
+    async def publish(payload, info, id):
         """Called to notify the client."""
 
         # Here `payload` contains the `payload` from the `broadcast()`
@@ -29,5 +36,6 @@ class PollSubscription(channels_graphql_ws.Subscription):
         # if you wish to suppress the notification to a particular
         # client. For example, this allows to avoid notifications for
         # the actions made by this particular client.
-        time.sleep(0.5)  # TODO figure out why there is a race condition with the DB
-        return PollSubscription(pollById=models.Poll.objects.get(pk=id))
+        await asyncio.sleep(0.5)  # TODO figure out why there is a race condition with the DB
+        poll = await PollSubscription.get_poll(id)
+        return PollSubscription(pollById=poll)
